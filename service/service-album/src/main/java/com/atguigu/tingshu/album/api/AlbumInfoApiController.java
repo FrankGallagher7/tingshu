ca2d2fb68caa02a5c9c5219e3ebbcfbdb6f99629
FrankGallagher7 <1983519800@qq.com>
2025-02-17 11:53:17 +0800

    专辑分页查询

--- service/service-album/src/main/java/com/atguigu/tingshu/album/api/AlbumInfoApiController.java
+++ service/service-album/src/main/java/com/atguigu/tingshu/album/api/AlbumInfoApiController.java
@@ -3,13 +3,13 @@ package com.atguigu.tingshu.album.api;
 import com.atguigu.tingshu.album.service.AlbumInfoService;
 import com.atguigu.tingshu.common.result.Result;
 import com.atguigu.tingshu.common.util.AuthContextHolder;
+import com.atguigu.tingshu.query.album.AlbumInfoQuery;
 import com.atguigu.tingshu.vo.album.AlbumInfoVo;
+import com.atguigu.tingshu.vo.album.AlbumListVo;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "专辑管理")
 @RestController
@@ -20,6 +20,30 @@ public class AlbumInfoApiController {
 
 	@Autowired
 	private AlbumInfoService albumInfoService;
+
+
+	/**
+	 * 查看当前用户专辑分页列表
+	 * api/album/albumInfo/findUserAlbumPage/{page}/{limit}
+	 * @param page
+	 * @param limit
+	 * @param albumInfoQuery
+	 * @return
+	 */
+	@PostMapping("/albumInfo/findUserAlbumPage/{page}/{limit}")
+	public Result<Page<AlbumListVo>> findUserAlbumList(@PathVariable Long page,
+													   @PathVariable Long limit,
+													   @RequestBody AlbumInfoQuery albumInfoQuery) {
+		// 封装分页对象
+		Page<AlbumListVo> albumListVoPage = new Page<>(page, limit);
+		// 获取用户ID
+		Long userId = AuthContextHolder.getUserId();
+		albumInfoQuery.setUserId(userId);
+
+		albumListVoPage = albumInfoService.findUserAlbumPage(albumListVoPage,albumInfoQuery);
+
+		return Result.ok(albumListVoPage);
+	}
 
 	/**
 	 * 新增专辑
